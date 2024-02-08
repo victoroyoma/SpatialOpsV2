6dd7530a6e35fdf90081bb974f9b211f478b6177
victoroyoma <victoroyoma@gmail.com>
2024-02-08 15:08:55 +0100

    dbconfig update

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -8,5 +8,11 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: "postgres",
     port: process.env.DB_PORT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // This allows for self-signed certificates
+      },
+    },
   },
 };
