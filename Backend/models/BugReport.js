e96948ab8f2b240f62c9bcfe7e12d76a852420ee
victoroyoma <victoroyoma@gmail.com>
2024-03-18 16:09:54 +0100

    device log rework v3

--- Backend/models/BugReport.js
+++ Backend/models/BugReport.js
@@ -1,18 +1,18 @@
-// module.exports = (sequelize, DataTypes) => {
-//   const BugReport = sequelize.define(
-//     "BugReport",
-//     {
-//       id: {
-//         type: DataTypes.INTEGER,
-//         primaryKey: true,
-//         autoIncrement: true,
-//       },
-//       errorMessage: DataTypes.TEXT,
-//       component: DataTypes.STRING,
-//       lineNumber: DataTypes.INTEGER,
-//       occurredAt: DataTypes.DATE,
-//     },
-//     {}
-//   );
-//   return BugReport;
-// };
+module.exports = (sequelize, DataTypes) => {
+  const BugReport = sequelize.define(
+    "BugReport",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      errorMessage: DataTypes.TEXT,
+      component: DataTypes.STRING,
+      lineNumber: DataTypes.INTEGER,
+      occurredAt: DataTypes.DATE,
+    },
+    {}
+  );
+  return BugReport;
+};
--- Frontend/src/components/BugReportDialog.jsx
+++ Frontend/src/components/BugReportDialog.jsx
@@ -7,35 +7,73 @@ import {
   TextField,
   Button,
 } from "@mui/material";
+import axios from "axios";
 
 function BugReportDialog({ open, onClose }) {
-  const [description, setDescription] = useState("");
+  const [bugReport, setBugReport] = useState({
+    errorMessage: "",
+    component: "",
+    lineNumber: "",
+    occurredAt: "",
+  });
 
-  const handleSubmit = () => {
-    // TODO: Integrate HTML scene capture functionality here
+  const handleChange = (e) => {
+    setBugReport({ ...bugReport, [e.target.name]: e.target.value });
+  };
 
-    // TODO: Submit the bug report along with the scene capture to the backend
-    console.log("Bug Description:", description);
-    // Close dialog after submitting
+  const handleSubmit = async () => {
+    // TODO: Submit the bug report to the backend
+    await axios.post("/api/bug-reports", bugReport);
     onClose();
   };
 
   return (
-    <Dialog open={open} onClose={onClose}>
+    <Dialog open={open} onClose={onClose} fullWidth maxWidth="sm">
       <DialogTitle>File a Bug</DialogTitle>
       <DialogContent>
         <TextField
-          autoFocus
           margin="dense"
-          id="description"
-          label="Bug Description"
+          name="errorMessage"
+          label="Error Message"
           type="text"
           fullWidth
           variant="outlined"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          value={bugReport.errorMessage}
+          onChange={handleChange}
         />
-        {/* Additional fields as needed */}
+        <TextField
+          margin="dense"
+          name="component"
+          label="Component"
+          type="text"
+          fullWidth
+          variant="outlined"
+          value={bugReport.component}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          name="lineNumber"
+          label="Line Number"
+          type="number"
+          fullWidth
+          variant="outlined"
+          value={bugReport.lineNumber}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          name="occurredAt"
+          label="Occurred At"
+          type="datetime-local"
+          fullWidth
+          variant="outlined"
+          value={bugReport.occurredAt}
+          onChange={handleChange}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose} color="primary">
