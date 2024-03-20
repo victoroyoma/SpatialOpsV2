c3b2247eb6e5244ef55a1d1ed4a9a64ca133c9fb
victoroyoma <victoroyoma@gmail.com>
2024-03-20 22:37:18 +0100

    added try and catch to debug dialog

--- Backend/controllers/bugReportController.js
+++ Backend/controllers/bugReportController.js
@@ -7,29 +7,24 @@ exports.getAllBugReports = async (req, res) => {
     res.json({ success: true, data: bugReports });
   } catch (error) {
     console.error("Fetch Error:", error);
-    res
-      .status(500)
-      .json({
-        success: false,
-        message: "Failed to fetch bug reports",
-        error: error.message,
-      });
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch bug reports",
+      error: error.message,
+    });
   }
 };
 
-// Create a new bug report
 exports.createBugReport = async (req, res) => {
-  try {
-    const bugReport = await BugReport.create(req.body);
-    res.status(201).json({ success: true, data: bugReport });
-  } catch (error) {
-    console.error("Creation Error:", error);
-    res
-      .status(500)
-      .json({
-        success: false,
-        message: "Failed to create bug report",
-        error: error.message,
-      });
-  }
+  // try {
+  //   const bugReport = await BugReport.create(req.body);
+  //   res.status(201).json({ success: true, data: bugReport });
+  // } catch (error) {
+  //   console.error("Creation Error:", error);
+  //   res.status(500).json({
+  //     success: false,
+  //     message: "Failed to create bug report",
+  //     error: error.message,
+  //   });
+  // }
 };
