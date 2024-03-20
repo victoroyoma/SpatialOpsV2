7e0726318e4f00ea34eec19eec40161cff7280e8
victoroyoma <victoroyoma@gmail.com>
2024-03-20 21:18:22 +0100

    bugreport backend v6

--- Backend/routes/bugReportRoutes.js
+++ Backend/routes/bugReportRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const BugReportController = require("../controllers/BugReportController");
 
-router.get("/bug-reports", BugReportController.getAllBugReports);
-router.post("/bug-reports", BugReportController.createBugReport);
+router.get("/api/bug-reports", BugReportController.getAllBugReports);
+router.post("/api/bug-reports", BugReportController.createBugReport);
 
 module.exports = router;
