64a65d0635ef8daab5eaafc601b1e779923cbb44
victoroyoma <victoroyoma@gmail.com>
2024-03-20 19:52:04 +0100

    bugreport backend v4

--- Backend/routes/bugReportRoutes.js
+++ Backend/routes/bugReportRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const BugReportController = require("../controllers/bugReportController");
+const BugReportController = require("../controllers/BugReportController");
 
 router.get("/bug-reports", BugReportController.getAllBugReports);
 router.post("/bug-reports", BugReportController.createBugReport);
